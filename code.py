bca2b33e46b16bf0eaa05946052a0c5968ab3700
Sammy Jolene <sammyshorey@gmail.com>
2022-11-24 06:55:04 -0500

    before checking for winner

--- code.py
+++ code.py
@@ -1,4 +1,5 @@
 board = {'1':'1', '2':'2', '3':'3', '4':'4', '5':'5', '6':'6', '7':'7', '8':'8', '9':'9'}
+playing = True
 
 def print_board(board):
     print(board['1'] + " | " + board['2'] + " | " + board['3'])
@@ -7,20 +8,42 @@ def print_board(board):
     print("- - - - - ")
     print(board['7'] + " | " + board['8'] + " | " + board['9'])
 
+def winner():
+    if board['1'] == board['2'] == board['3']:
+        playing = False
+    if board['4'] == board['5'] == board['6']:
+        playing = False
+    if board['7'] == board['8'] == board['9']:
+        playing = False
+    if board['1'] == board['4'] == board['7']:
+        playing = False
+    if board['2'] == board['5'] == board['8']:
+        playing = False
+    if board['3'] == board['6'] == board['9']:
+        playing = False
+    if board['1'] == board['5'] == board['9']:
+        playing = False
+    if board['3'] == board['5'] == board['7']:
+        playing = False
 
-turn = 0
 player = 2
 
-for n in range(9):
-    if player % 2 ==0:
-        letter = "X"
-    else: letter = "O"
+while playing:
+    for n in range(9):
+        if player % 2 ==0:
+            letter = "X"
+        else: letter = "O"
+        print_board(board)
+        while True:
+            move = input("It's player " + letter + "\'s turn. What space do you want to choose?")
+            if board[str(move)] != 'X' and board[str(move)] != 'O':
+                board[move] = letter
+                print(board)
+                break
+            else:
+                print("That space is already filled, choose another space.")
+                continue
+        player +=1
     print_board(board)
-    print("It's player " + letter + " turn. What space do you want to choose?")
-    move = input()
-    if board[str(move)] != "X" or "O":
-        board[move] = letter
-    else:
-        print("That space is already filled, choose another space.")
-    player +=1
+    winner(check)
 
