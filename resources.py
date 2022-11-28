ff74967996f226d5345f86b6b21a1d47ddca2e2f
Sammy Jolene <sammyshorey@gmail.com>
2022-11-28 05:17:06 -0500

    Add won variable

--- resources.py
+++ resources.py
@@ -11,25 +11,32 @@ winner = False
 
 def winner_check(board):
     if board['1'] == board['2'] == board['3']:
-        print('Player ' + str(board['1']) + ' is the winner!')
+        won = str(board['1']) 
+        print('Player ' + won + ' is the winner!')
         return True
     elif board['4'] == board['5'] == board['6']:
-        print('Player ' + str(board['4']) + ' is the winner!')
+        won = str(board['4'])
+        print('Player ' + won + ' is the winner!')
         return True
     elif board['7'] == board['8'] == board['9']:
-        print('Player ' + str(board['7']) + ' is the winner!')
+        won = str(board['7'])
+        print('Player ' + won + ' is the winner!')
         return True
     elif board['1'] == board['4'] == board['7']:
-        print('Player ' + str(board['1']) + ' is the winner!')
+        won = str(board['1'])
+        print('Player ' + won + ' is the winner!')
         return True
     elif board['3'] == board['6'] == board['9']:
-        print('Player ' + str(board['3']) + ' is the winner!')
+        won = str(board['3'])
+        print('Player ' + won + ' is the winner!')
         return True
     elif board['1'] == board['5'] == board['9']:
-        print('Player ' + str(board['1']) + ' is the winner!')
+        won = str(board['1'])
+        print('Player ' + won + ' is the winner!')
         return True
     elif board['3'] == board['5'] == board['7']:
-        print('Player ' + str(board['3']) + ' is the winner!')
+        won = str(board['3'])
+        print('Player ' + won + ' is the winner!')
         return True
     else: return False
     
